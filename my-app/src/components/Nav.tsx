dfdd52a9ef1266fdcffcb7170dbcd83940395b69
mwala <mwalamulenga.zt@gmail.com>
2023-01-25 13:54:16 +0200

    tailwind added, components file created and nav component started

---
+++ my-app/src/components/Nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function nav() {
+  return (
+    <div className='flex'>
+        <nav>
+        <h1 className='font-Merriweather'>Meme</h1>
+        </nav>
+    </div>
+  )
+}
+
+export default nav
---
+++ my-app/tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    colors: {
+      primary: "#F0F0F0",
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px"
+    }
+  },
+}
