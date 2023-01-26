fc19f314cbe1b507ec306c63653171785f8079a4
lwandokasuba <lwando@thinkzambia.org>
2023-01-26 20:29:10 +0200

    moved files to main folder

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './style';
+
+import { Nav } from './components';
+
+function App() {
+  return (
+    <div className={`flex flex-col items-center w-full`}>
+      {/* Navigation */}
+      <div className={`${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Nav />
+        </div>
+      </div>
+
+      {/* Other sections */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+        primary: "#F0F0F0",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
